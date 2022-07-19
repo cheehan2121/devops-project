79544dbc3d6a941e9f978761f3aa79b6992820a6
Player <LSSLIFU21.21@ichat.sp.edu.sg>
2022-07-19 20:28:14 +0800

    new hal file

--- src/App/main.py
+++ src/App/main.py
@@ -2,7 +2,7 @@ import time
 
 from hal import hal_led as led
 from hal import hal_lcd as LCD
-
+from hal import hal_rfid_reader as rfid
 
 
 
@@ -10,8 +10,9 @@ from hal import hal_lcd as LCD
 def main():
     #initialization of HAL modules
     led.init()
+    rfid.init()
     lcd = LCD.lcd()
-
+    rfid.init()
     lcd.lcd_clear()
 
     lcd.lcd_display_string("Mini-Project", 1)
@@ -24,6 +25,8 @@ def main():
         led.set_output(1, 0)
         time.sleep(1)
 
+    scan = rfid.SimpleMFRC522
+    print(scan)
 
 if __name__ == '__main__':
     main()
