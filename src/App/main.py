bf262576f709e30428c2033b2f4fb97902358577
Tan chee han <cheehan21.21@ichat.sp.edu.sg>
2022-07-19 21:13:50 +0800

    try

--- src/App/main.py
+++ src/App/main.py
@@ -2,28 +2,36 @@ import time
 
 from hal import hal_led as led
 from hal import hal_lcd as LCD
-
-
-
+from hal import hal_servo as servo
+from hal import hal_usonic as detect
+from hal import hal_buzzer as alarm
 
 
 def main():
     #initialization of HAL modules
+    intruder_detected=0
+    detect.init()
+    alarm.init()
     led.init()
     lcd = LCD.lcd()
-
+    servo.init()
     lcd.lcd_clear()
 
+
+    while True:
+        print(detect.get_distance())
+        if(detect.get_distance()<5):
+            print("intruder detected")
+            intruder_detected=1
+            break
+
+
     lcd.lcd_display_string("Mini-Project", 1)
-    lcd.lcd_display_string("Template", 2)
+    lcd.lcd_display_string("Templatb", 2)
 
-    while(True):
-        led.set_output(1, 1)
-        time.sleep(1)
 
-        led.set_output(1, 0)
-        time.sleep(1)
-
+    while(intruder_detected==1):
+        alarm.short_beep(0.5)
 
 if __name__ == '__main__':
     main()
