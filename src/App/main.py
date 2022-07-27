6b0e192ed16618594b2e8d833ee275130ae9db45
Player <LSSLIFU21.21@ichat.sp.edu.sg>
2022-07-27 17:41:23 +0800

    keypad and more hal files

--- src/App/main.py
+++ src/App/main.py
@@ -2,7 +2,23 @@ import time
 
 from hal import hal_led as led
 from hal import hal_lcd as LCD
-from hal import hal_rfid_reader as rfid
+from hal import hal_rfid_reader as rfid_reader
+from hal import hal_keypad as keypad
+from threading import Thread
+def key_pressed(key):
+    password=[]
+    password.append(key)
+
+    print(password)
+
+
+def keypad_thread():
+
+    for i in range(6) :
+        key=[]
+        key=keypad.get_key()
+        i+=i+1
+    print(key)
 
 
 
@@ -10,23 +26,39 @@ from hal import hal_rfid_reader as rfid
 def main():
     #initialization of HAL modules
     led.init()
-    rfid.init()
+
     lcd = LCD.lcd()
-    rfid.init()
+
     lcd.lcd_clear()
 
     lcd.lcd_display_string("Mini-Project", 1)
     lcd.lcd_display_string("Template", 2)
+    lcd.lcd_clear()
 
-    while(True):
-        led.set_output(1, 1)
-        time.sleep(1)
 
-        led.set_output(1, 0)
-        time.sleep(1)
+    keypad.init(key_pressed)
 
-    scan = rfid.SimpleMFRC522
-    print(scan)
+# Start the keypad scanning which will run forever in an infinite while(True) loop in a new Thread "keypad_thread"
+    keypad_thread = Thread(target=keypad.get_key)
+    keypad_thread.start()
+    reader = rfid_reader.init()
 
+    while True:
+        id = reader.read_id_no_block()
+        id = str(id)
+
+        print("RFID card ID = " + id)
+        if id=="None":
+            lcd.lcd_display_string("Press KeyCard")
+
+        elif id == '988654710544':
+
+            led.set_output(1, 0)
+            lcd.lcd_display_string("Door unlocked")
+
+        else:
+            lcd.lcd_clear()
+            lcd.lcd_display_string("Door locked")
+            break
 if __name__ == '__main__':
     main()
