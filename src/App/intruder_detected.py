5e1eb075b2d5e148ce598b207e78aa984b30ba59
Tan chee han <cheehan21.21@ichat.sp.edu.sg>
2022-08-01 13:55:53 +0800

    intrusion detected fearture

--- src/App/intruder_detected.py
+++ src/App/intruder_detected.py
@@ -1,9 +1,23 @@
+import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'hal'))
 
-import time
-from threading import Thread
-import hal_lcd as LCD
-import hal_usonic as detect
 import hal_buzzer as alarm
 import hal_input_switch as lock
+import hal_usonic as detect
+from threading import Thread
+import time
+def alarmsound():
 
+    while(True):
+        print(detect.get_distance())
+        print(lock.read_slide_switch())
+        #switch to the right is 0 to the left is 1
+        #1 is lock 0 is unlock
+        if(detect.get_distance()>10):
+            if(lock.read_slide_switch()==1):
+                print("intruder detected")
+                alarm.short_beep(1)
+
+def alarmstart():
+    threadintrusion = Thread(target=alarmsound)
+    threadintrusion.start()
--- src/App/main.py
+++ src/App/main.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'hal'))
 import intruder_detected
+import keypad
 import time
 from threading import Thread
 import hal_led as led
@@ -9,10 +10,9 @@ import hal_servo as servo
 import hal_usonic as detect
 import hal_buzzer as alarm
 import hal_input_switch as lock
-import hal_keypad as keypad
 
 def alarmsound():
-    while(True):
+    while(1==1):
         print(detect.get_distance())
         print(lock.read_slide_switch())
         #switch to the right is 0 to the left is 1
@@ -21,13 +21,14 @@ def alarmsound():
             if(lock.read_slide_switch()==1):
              print("intruder detected")
              alarm.short_beep(1)
+
 def main():
     #initialization of HAL modules
     global Run_Multithread
 
     Run_Multithread = True
 
-    intruder_detected=0
+
     lock.init()
     detect.init()
     alarm.init()
@@ -35,13 +36,12 @@ def main():
     lcd = LCD.lcd()
     servo.init()
     lcd.lcd_clear()
-
     lcd.lcd_display_string("Mini-Project", 1)
     lcd.lcd_display_string("Template", 2)
-    threadintrusion =Thread(target=alarmsound)
-    threadintrusion.start()
-    print("pp")
-
+    #threadintrusion =Thread(target=alarmsound())
+    #threadintrusion.start()
+    #intruder_detected.alarmstart()
+    keypad.test()
 
 if __name__ == '__main__':
     main()
