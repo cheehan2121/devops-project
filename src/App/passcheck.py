e20d4b6a55f3710e19beac0a0c82db5ab8130efa
Tan chee han <cheehan21.21@ichat.sp.edu.sg>
2022-08-16 14:09:11 +0800

    docs update

---
+++ src/App/passcheck.py
@@ -0,0 +1,28 @@
+password1 = [1,2,3,4]
+password2 = [2,3,4,5]
+password3 = [3,4,5,6]
+password4 = [4,5,6,7]
+
+id ='1052230762465'
+def test_password1(key):
+    if key == password1:
+        return True
+
+def test_password2(key):
+    if key == password2:
+        return True
+def test_password3(key):
+    if key == password3:
+        return True
+def test_password3(key):
+    if key == password3:
+        return True
+def test_password4(key):
+    if key == password4:
+        return True
+def test_RFID(RFID):
+    if RFID == id:
+        return True
+def test_RFID_fail(RFID):
+    if RFID != id:
+        return False
